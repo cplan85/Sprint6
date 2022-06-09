2759aa5f3ebd1fd69299431cb9a8b250e264db71
Carlos Planchart <carlos_planchart@live.com>
2022-06-09 13:00:25 +0200

    up to exercise 4

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -7,6 +7,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class EscenaComponent implements OnInit {
 
+  currentSentence ="";
+
   @Input() public parentData: string[] = [""];
   constructor() { }
 
