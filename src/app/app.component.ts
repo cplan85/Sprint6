98e45f36bd77af0496f8f9352a151496ea1a84f9
Carlos Planchart <carlos_planchart@live.com>
2022-06-09 10:49:44 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sprint6-app';
+
+  public history: string[] = [
+    "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
+    "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
+    "L'heroi va decidir travessar la porta que el portava a casa",
+    "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ..."
+  ]
+  
+
+  // use ng g c <appname> to create new app. Use optional -s -t flags to remove styling or template. 
+  title = 'Sprint 6 App';
+}
+
+interface HistoryItem {
+  text: string;
 }
---
+++ src/app/escena/escena.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-escena',
+  templateUrl: './escena.component.html',
+  styleUrls: ['./escena.component.css']
+})
+export class EscenaComponent implements OnInit {
+
+  @Input() public parentData: string[] = [""];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
